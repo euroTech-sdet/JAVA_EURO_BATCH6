214c8fc64757091a8845ad58b01071edda3cb9a5
offaruk <faruk.topak@hotmail.com>
2022-11-01 00:05:11 +0000

    Day8

---
+++ src/days/day6/Exercises.java
@@ -0,0 +1,10 @@
+package days.day6;
+
+public class Exercises {
+    public static void main(String[] args) {
+        int x = 3;
+        x = x + 2;
+        x = x + x;
+        System.out.println("x = " + x);
+    }
+}
