bde9a97afac08399437d3845b1bb77d6365dbc7f
offaruk <faruk.topak@hotmail.com>
2022-11-16 22:25:29 +0000

    nested for loops

---
+++ src/days/day18/Nested4.java
@@ -0,0 +1,23 @@
+package days.day18;
+
+public class Nested4 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("hello");
+            for (int n = 0; n < 4; n++) {
+                System.out.println("inner");
+            }
+        }
+        // prime numbers 2-50 (nested loops)
+        // fibonacci numbers
+        // print alphabet
+        // how to print factors of the numbers
+        // Write a program to find the factorial value of any number
+        // print diamond- other patterns
+
+
+
+
+    }
+}
