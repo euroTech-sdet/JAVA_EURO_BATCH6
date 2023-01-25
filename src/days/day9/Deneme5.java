f2daf363e816605e40b01280548672489d335a30
offaruk <faruk.topak@hotmail.com>
2023-01-25 21:08:20 +0000

    inheritance

---
+++ src/days/day9/Deneme5.java
@@ -0,0 +1,11 @@
+package days.day9;
+
+public class Deneme5 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a++ > b && 1 > b++ + b * a);
+        System.out.println( 1 & 19999);
+
+    }
+}
