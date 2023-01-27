811ed6220d9e2b6bd5ec741fd986b1898c48d514
Talha <talha43gs@gmail.com>
2023-01-27 21:06:26 +0000

    Day 55 Uploaded

---
+++ src/days/day55/Quiz5Review.java
@@ -0,0 +1,26 @@
+package days.day55;
+
+import java.util.Arrays;
+
+public class Quiz5Review {
+
+
+    public static void main(String[] args) {
+
+        int[]test= {44,11,22,33};
+        System.out.println("Arrays.binarySearch(test,11) = " + Arrays.binarySearch(test, 11));
+
+
+        Integer.parseInt("23");
+
+        Integer.valueOf("23");
+
+        Integer.toString(23);
+
+        Integer obj = 2;
+
+        System.out.println("obj.intValue() = " + obj.intValue());
+
+
+    }
+}
